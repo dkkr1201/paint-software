de4beb07bd6aff9b5ab113c893960559595607a3
Deepak <kmrdeepak1201@gmail.com>
2026-03-29 23:51:43 +0530

    feat: add Window, DrawingContext, DialogBox, DataController, Event

---
+++ src/main/java/com/paint/ConsoleWindow.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.paint;
+
+/**
+ *
+ * @author 91941
+ */
+
+public class ConsoleWindow extends Window {
+    public ConsoleWindow(String title) { super(title); }
+}
